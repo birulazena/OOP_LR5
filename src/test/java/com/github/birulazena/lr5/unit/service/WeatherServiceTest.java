13f9844eab02a1346de60e480fad70d6d1bc7c22
birulya.zhenka <birulya.zhenka@gmail.com>
2026-05-01 23:25:53 +0300

    add tests

--- src/test/java/com/github/birulazena/lr5/unit/service/WeatherServiceTest.java
+++ src/test/java/com/github/birulazena/lr5/unit/service/WeatherServiceTest.java
@@ -4,7 +4,9 @@ import com.github.birulazena.lr5.client.OpenWeatherClient;
 import com.github.birulazena.lr5.client.WeatherClientFactory;
 import com.github.birulazena.lr5.client.WeatherDataClient;
 import com.github.birulazena.lr5.client.type.WeatherProviderType;
+import com.github.birulazena.lr5.model.Coordinates;
 import com.github.birulazena.lr5.model.CurrentWeather;
+import com.github.birulazena.lr5.repository.WeatherRepository;
 import com.github.birulazena.lr5.service.WeatherService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +16,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +25,9 @@ public class WeatherServiceTest {
 
     @Mock
     private WeatherClientFactory weatherClientFactory;
+
+    @Mock
+    private WeatherRepository weatherRepository;
 
     @InjectMocks
     private WeatherService weatherService;
@@ -38,6 +44,9 @@ public class WeatherServiceTest {
         Mockito.when(weatherClientFactory.getClient(type))
                 .thenReturn(mockClient);
 
+        Mockito.when(mockClient.getCurrentTemperature(lat, lon))
+                .thenReturn(expectedTemp);
+
         CurrentWeather result = weatherService.getCurrentWeather(lat, lon, type);
 
         assertNotNull(result);
@@ -45,5 +54,44 @@ public class WeatherServiceTest {
 
         Mockito.verify(weatherClientFactory).getClient(type);
         Mockito.verify(mockClient).getCurrentTemperature(lat, lon);
+    }
+
+    @Test
+    void getCurrentWeatherWithCity_ReturnsSuccessResponse() {
+        String city = "Minsk";
+        WeatherProviderType type = WeatherProviderType.GOOGLE_WEATHER;
+        Coordinates coordinates = new Coordinates(new BigDecimal("10.0"), new BigDecimal("20.0"));
+        BigDecimal expectedTemp = new BigDecimal("25.5");
+
+        WeatherDataClient mockClient = Mockito.mock(WeatherDataClient.class);
+
+        Mockito.when(weatherRepository.getCoordinatesByCity(city))
+                        .thenReturn(Optional.of(coordinates));
+
+        Mockito.when(weatherClientFactory.getClient(type))
+                .thenReturn(mockClient);
+
+        Mockito.when(mockClient.getCurrentTemperature(coordinates.lat(), coordinates.lon()))
+                .thenReturn(expectedTemp);
+
+        CurrentWeather result = weatherService.getCurrentWeather(city, type);
+
+        assertNotNull(result);
+        assertEquals(result, expectedTemp);
+    }
+
+    @Test
+    void getCurrentWeatherWithCity_ReturnsCoordinatesErrorResponse() {
+        String city = "Mogilev";
+        WeatherProviderType type = WeatherProviderType.GOOGLE_WEATHER;
+
+        Mockito.when(weatherRepository.getCoordinatesByCity(city))
+                .thenReturn(Optional.empty());
+
+        CoordinatesNotFoundException exception = assertThrows(CoordinatesNotFoundException.class, () -> {
+            weatherService.getCurrentWeather(city, type);
+        });
+
+        Mockito.verifyNoInteractions(weatherClientFactory);
     }
 }
