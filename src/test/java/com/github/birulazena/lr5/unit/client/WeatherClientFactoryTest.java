1b213df1c00cca397204c8d676d067b7f2e371ec
birulya.zhenka <birulya.zhenka@gmail.com>
2026-05-01 17:03:11 +0300

    add new tests

---
+++ src/test/java/com/github/birulazena/lr5/unit/client/WeatherClientFactoryTest.java
@@ -0,0 +1,47 @@
+package com.github.birulazena.lr5.unit.client;
+
+import com.github.birulazena.lr5.client.WeatherDataClient;
+import com.github.birulazena.lr5.client.type.WeatherProviderType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class WeatherClientFactoryTest {
+
+    @Mock
+    private WeatherDataClient mockOpenWeather;
+
+    private WeatherClientFactory weatherClientFactory;
+
+    @BeforeEach
+    void setUp() {
+        Mockito.lenient().when(mockOpenWeather.getProviderType()).thenReturn(WeatherProviderType.OPEN_WEATHER);
+
+        weatherClientFactory = new WeatherClientFactory(List.of(mockOpenWeather));
+    }
+
+    @Test
+    void shouldReturnCorrectClientByProviderType() {
+        WeatherDataClient result = weatherClientFactory.getClient(WeatherProviderType.OPEN_WEATHER);
+
+        assertNotNull(result);
+        assertEquals(mockOpenWeather, result);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenProviderNotFound() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            weatherClientFactory.getClient(WeatherProviderType.GOOGLE_WEATHER);
+        });
+
+        assertTrue(exception.getMessage().contains("Provider is not supported"));
+    }
+}
